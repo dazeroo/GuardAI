9d77bd93acc7ac49190dbbe5d6b131414b6f5011
cdsgs0905 <cdsgs0905@gmail.com>
2025-09-29 18:46:42 +0900

    Update vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
-
+  },
 
   server: {
     proxy: {
@@ -21,5 +21,5 @@ export default defineConfig({
     // watch: {
     //   usePolling: true,
     //  },
-  },
+ // },
 })
