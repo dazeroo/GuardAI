40ce5412a09f43f36e7cd729b5d2d4a85c5a53ad
cdsgs0905 <cdsgs0905@gmail.com>
2025-09-29 18:24:02 +0900

    Update MgmtConsultingPanel.tsx

--- Frontend/src/components/ui/MgmtConsultingPanel.tsx
+++ Frontend/src/components/ui/MgmtConsultingPanel.tsx
@@ -986,7 +986,7 @@ export default function MgmtConsultingPanel() {
       setProgress(35);
 
       // Flask 백엔드 API 호출
-      const response = await fetch('http://192.168.0.63:3001', {
+      const response = await fetch('http://127.0.0.1:3001', {
         method: 'POST',
         body: formData,
       });
