6e32e2929bb31c672e937eb847fc133b24eaa962
dazeroo <kdofutu@gmail.com>
2025-10-10 21:11:32 +0900

    Integrate Tech-Consulting features

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/routers': {
-        target: 'http://0.0.0.0:8000',
+        target: 'http://127.0.0.1:8000',   //http://0.0.0.0:8000
         changeOrigin: true,
       },
     },
